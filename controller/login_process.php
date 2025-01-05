df8dfc091a847267414dc9208eb80825d486c19b
anouar el barry <elbarryanwar37@gmail.com>
2025-01-05 14:32:50 +0100

    get user role

--- controller/login_process.php
+++ controller/login_process.php
@@ -1,23 +1,33 @@
 <?php
 require_once 'classes/user.php';
-
+session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Validate input
     if (isset($_POST['email']) && isset($_POST['password'])) {
         // Trim and sanitize inputs
         $email = trim($_POST['email']);
         $password = $_POST['password'];
-
+      
+         // Create user instance
+        
+         
 
 $db = new Database();
 $db->getConnection();
         $is_logged = User::login($db,$email,$password);
         if ($is_logged) {
-            header('Location: ../view/user-dashboard.php');
+            $_SESSION["email"] = $email;
+            $_SESSION["password"] = $password;
+            $user_Data = User::getUserData($email);
+            $_SESSION['username'] = $user_Data["username"];
+            $_SESSION['userid'] = $user_Data["id"];
+            $_SESSION['userRole'] = $user_Data["role"];
+          
+            header('Location: ../view/project-manager-dashboard.php');
         }
         else{
            
-            echo 'error';
+            echo 'user not found';
         }
         
     }
